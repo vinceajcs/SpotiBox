ebc3005cbef83f3ef99b63fc7cdaaafa3754ae89
Vincent Stephen Huang <hvincentstephen@Vincents-MacBook-Pro.local>
2017-11-30 14:19:34 -0500

    Initial Commit

---
+++ SpotiBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpotiBox
+//
+//  Created by Vincent Stephen Huang on 11/30/17.
+//  Copyright © 2017 Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
